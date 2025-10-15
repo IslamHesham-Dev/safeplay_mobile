7577e929cea302bf04dcdfc4684eeeab156b5322
Islam Hesham Kamel Ali Ismail <islam.ismail@student.giu-uni.de>
2025-10-15 14:17:35 +0300

    Remove Firebase Functions to fix iOS build issues
    Eliminated all references to cloud_functions from dependencies, code, and plugin registration to resolve iOS build problems. The sendCredentialsEmail method is now disabled, and parents must manually share credentials.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import audioplayers_darwin
 import cloud_firestore
-import cloud_functions
 import connectivity_plus
 import file_selector_macos
 import firebase_analytics
@@ -26,7 +25,6 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
